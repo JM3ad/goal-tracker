14daf7039761dc2b8c63d62faac837e4f9371e7e
Jack Mead <jack-mead@hotmail.co.uk>
2023-01-01 17:00:10 +0000

    Tailwind working

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,14 @@
 import ReactDOM from "react-dom";
 import React from "react";
+
+import "src/app.css";
 import Fake from "src/sub_module/fake";
 // @ts-ignore
 import { MY_ENV_VARIABLE } from "env";
 
 const App = () => {
     return (
-        <h1>
+        <h1 className="text-2xl">
             This is my React app!
             {MY_ENV_VARIABLE}
             <Fake />
